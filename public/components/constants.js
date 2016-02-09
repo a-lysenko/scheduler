0ad58a5932fa3d7aa8028377f4b47959afe0f566
Oleksii Lysenko <Oleksii_Lysenko@epam.com>
2016-02-09 13:10:36 +0200

    removed redundant constants

--- public/components/constants.js
+++ public/components/constants.js
@@ -1,11 +1,9 @@
 (function () {
     angular
         .module('schedulerApp')
-        .constant('AMOUNT_HOURS_IN_DAY', 12)
         .constant('HOURS_INTERVAL', {
             from: 0,
             to: 12
         })
-        .constant('AMOUNT_DAYS_IN_WEEK', 7)
         .constant('HOUR_BLOCK_HEIGHT_PX', 25);
 })();
